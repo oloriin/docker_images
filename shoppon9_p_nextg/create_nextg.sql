26e10a3490f59b22825fd92740141abaf7533107
oloriin <mihaz2003@mail.ru>
2016-06-22 16:21:32 +1000

    Update create_nextg.sql

--- shoppon9_p_nextg/create_nextg.sql
+++ shoppon9_p_nextg/create_nextg.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS `shoppon9_p_nextg` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `shoppon9_p_nextg` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `shoppon9_p_nextg`;
